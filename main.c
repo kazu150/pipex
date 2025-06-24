0d33f07da932bd2b81a7b32bce22c090678515eb
Kazushi Isogai <kaisogai@c2r3s1.42tokyo.jp>
2025-06-24 18:03:56 +0900

    :

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vscode <vscode@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kaisogai <kaisogai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 23:16:06 by vscode            #+#    #+#             */
-/*   Updated: 2025/06/24 16:45:13 by vscode           ###   ########.fr       */
+/*   Updated: 2025/06/24 17:30:06 by kaisogai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,88 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void	build_args(char *args[4], char *command)
+void	build_args(char *args[4], char *input_command)
 {
 	args[0] = "sh";
 	args[1] = "-c";
-	args[2] = command;
+	args[2] = input_command;
 	args[3] = NULL;
 }
+
+int	exec_output_child_process(char **args, char *output_command,
+		char *output_filename)
+{
+	int	status;
+	int	fd;
+	int	tmp_fd;
+
+	args[2] = output_command;
+	tmp_fd = open("tmp", O_RDONLY);
+	if (tmp_fd == -1)
+		return (1);
+	dup2(tmp_fd, STDIN_FILENO);
+	if (tmp_fd == -1)
+		return (1);
+	fd = open(output_filename, O_WRONLY | O_CREAT, 0644);
+	if (fd == -1)
+		return (1);
+	fd = dup2(fd, STDOUT_FILENO);
+	if (fd == -1)
+		return (1);
+	execve("/bin/sh", args, 0);
+	return (0);
+}
+
+int	exec_output_parent_process(pid_t pid)
+{
+	int	status;
+
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+		unlink("./tmp");
+	return (0);
+}
+
+int	exec_input_child_process(char **args)
+{
+	int	fd;
+	int	return_value;
+
+	fd = open("tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd == -1)
+		return (1);
+	fd = dup2(fd, STDOUT_FILENO);
+	if (fd == -1)
+		return (1);
+	return_value = execve("/bin/sh", args, 0);
+	if (return_value == -1)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int	exec_input_parent_process(pid_t pid, char **args, char *output_command,
+		char *output_filename)
+{
+	int	status;
+	int	fd;
+	int	tmp_fd;
+
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+	{
+		pid = fork();
+		if (pid < 0)
+			return (1);
+		if (pid == 0)
+			exec_output_child_process(args, output_command, output_filename);
+		else
+			exec_output_parent_process(pid);
+	}
+	return (0);
+}
+
 //  command: ./a.out file1 "grep a1" |  "wc -l" file2
 int	main(int argc, char **argv)
 {
@@ -33,48 +108,21 @@ int	main(int argc, char **argv)
 	int		status;
 	int		tmp_fd;
 
-	build_args(args, argv[2]);
-	if (argc < 5)
+	if (argc != 5)
 		return (1);
+	build_args(args, argv[2]);
 	fd = open(argv[1], O_RDONLY);
-	if (!fd)
+	if (fd == -1)
 		return (1);
 	fd = dup2(fd, STDIN_FILENO);
 	if (fd == -1)
 		return (1);
 	pid = fork();
-	if (pid < 0)
+	if (pid == -1)
 		return (1);
 	if (pid == 0)
-	{
-		fd = open("tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		dup2(fd, STDOUT_FILENO);
-		execve("/bin/sh", args, 0);
-	}
+		exec_input_child_process(args);
 	else
-	{
-		waitpid(pid, &status, 0);
-		if (WIFEXITED(status))
-		{
-			pid = fork();
-			if (pid < 0)
-				return (1);
-			if (pid == 0)
-			{
-				args[2] = argv[3];
-				tmp_fd = open("tmp", O_RDONLY);
-				fd = open(argv[4], O_WRONLY | O_CREAT, 0644);
-				dup2(fd, STDOUT_FILENO);
-				dup2(tmp_fd, STDIN_FILENO);
-				execve("/bin/sh", args, 0);
-			}
-			else
-			{
-				waitpid(pid, &status, 0);
-				if (WIFEXITED(status))
-					unlink("./tmp");
-			}
-		}
-	}
+		exec_input_parent_process(pid, args, argv[3], argv[4]);
 	return (0);
 }
